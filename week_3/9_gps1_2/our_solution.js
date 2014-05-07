81ab09e18b3ce86c1b1e734927323b5a81a02665
John Berry <jpberry0623@gmail.com>
2014-05-07 15:57:32 -0400

    Updated our_solution.js

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,22 +1,24 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. John Berry
+//  2. Joel Yawili
 
 
 // 1. "YOU SIGNED... WHO?!"
 
-
+adamSandler = {name: "Adam Sandler", age: 47, quote: "thats your home, are you too good for your home?"};
+    
 
 
 // 2. "Here they Come!"
-
-
+var Client = ("Adam Sandler", 47, "thats your home, are you too good for your home?");
 
 
 
 // 3. "TIME IS MONEY!"
+var Client = function(name, age, quote) {"name is" + name + ", " + "age is" + age + ", " + "quote is" + quote};
+
 
 //YOUR CODE HERE!
 
@@ -38,3 +40,15 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 
 //  Your Reflection:
+
+ELz and I had a good pairing session.  We struggled with this challenge as we were both new to javascript and had not had
+enough time to go trough the material to understand object oriented javascript which led us to spend most of hour session 
+attempting to make our function pass.  We were not able to come up with right solution, but I felt we asked a lot of good 
+questions of one another to describe what we were thinking and what concepts we were were stuck on.  Elz was the driver
+for the first half of the session and I drove for a shorter time at the end.  We went back and forth sharing ideas
+We were close to gaining a solution to how we should pass the information to our function, but all parties agreed we 
+needed more time op look over javascript concepts.  Even though we struggled it was very helpful going through the challenge 
+with Elz and recognizing similar areas we could bounce code solutions off one another to test. I used google and W3 schools
+as references during the challenge, but was unable to solve our problem in the short time we had.  We have agreed to
+put the code on ice so to speak and come back to it tomorrow night after further learning to tackle the challenge again.
+I enjoyed trying to solve code, but fear that javascript will not come as easily as HTML and CSS.  
