09b9a3d49225dd569db0b16f04dda573159c2df2
John Berry <jpberry0623@gmail.com>
2014-05-06 23:20:09 -0400

    Updated my_solution.js

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,19 +1,24 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself] on this challenge.
 
 
 
 
 // Pseudocode
 // 
-// 
-// 
-// 
+//Define variable secretNumber
+//Define variable password
+//Define variable allowedIn a boolean
+//Define variables members as a new arr
 
 
 // __________________________________________
 // Write your code below.
 
 
+var secretNumber = 7; 
+var password = "just open the door"; 
+var allowedIn = false; 
+var members = ["John", "Steve", "Alan", "Mary"] 
 
 
 
@@ -22,6 +27,9 @@
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
+I defined each variable simply using one line of code. If there is a way of making the code more simplified or DRYer I have
+not learned enough about javascript to do it.
+
 
 
 
@@ -29,11 +37,17 @@
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
-// 
-// 
+ This challnge was tough for me as javascript is totally new. I struggled at first to understand what the program was 
+ trying to accomplish because I had not read enough about variables, methods, and functions.  I had to stare at the code
+ for awhile before I realized that the error message the code was throwing was becasue of undefined variables, therefore 
+  I needed to go in and define those variables with the correct information to get the function to evalutate to true. 
+Going through and naming the variables was very helpful to understand how the varibales and funtions interact.  It felt 
+great to struggle with this at first and finally have the aha moment and see how simple the solution was.  Once I knew
+what I had to change it made me see the benefits of test driven development and going through the code piece
+by piece to see where it fails.  It definately shows me that I will need to put it a good chunk of time to learning javascript 
+as it does not seem to come to me nearly asnaturally as HTML and CSS.  My learning competency for this challenge is not 
+quite where it should be so I will go through the JS Intro a few more times to try and get a stronger foundation.
+
 
 
 // __________________________________________
